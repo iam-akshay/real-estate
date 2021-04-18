1c0dd90b7c8d727397544054376af5220ac7f212
Akshay Jain <jainakshay.aj98@gmail.com>
2021-04-18 12:16:57 +0530

    postgresql database setup

---
+++ README.md
@@ -0,0 +1,6 @@
+
+[Basic Django Cheat Sheet](https://github.com/lucrae/django-cheat-sheet)
+
+[Django Cheat Sheet](https://cheatography.com/tag/django/)
+
+[PostgreSQL Cheat Sheet](https://www.postgresqltutorial.com/postgresql-cheat-sheet/)
--- RealEstate/settings.py
+++ RealEstate/settings.py
@@ -32,6 +32,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pages',
+    'realtors',
+    'listings',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +79,11 @@ WSGI_APPLICATION = 'RealEstate.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'realestate',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost'
     }
 }
 

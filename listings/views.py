602284f8722a91a6e02d5ecdb8099dcb1b32b848
Akshay Jain <jainakshay.aj98@gmail.com>
2021-04-30 09:42:49 +0530

    Display dynamic data from db, paginate the data, humanize usage

--- listings/views.py
+++ listings/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
+from .models import Listing
+from django.core.paginator import Paginator
 
 def listings(request):
-    return render(request, 'listings/listings.html')
+    listings = Listing.objects.order_by("-list_date").filter(
+        is_published = True
+    )
+    paginator = Paginator(listings, 25)
+    current_page = request.GET.get("page", 1)
+    paginate_listings = paginator.get_page(current_page)
+    context = {
+        "listings": paginate_listings
+    }
+    return render(request, 'listings/listings.html', context)
 
-def listing(request):
+def listing(request, listing_id: int):
     return render(request, 'listings/listing.html')
 
 def search(request):

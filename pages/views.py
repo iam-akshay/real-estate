a82eaea69d2d4d72f908f786494bbe093c95b1e8
Akshay Jain <jainakshay.aj98@gmail.com>
2021-05-01 17:07:23 +0530

    Home & About page dynamic data and single listing page

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from listings.models import Listing
+from realtors.models import Realtor
 
 
 def page_home(request):
-    return render(request, 'pages/home.html')
+    listings = Listing.objects.order_by("-list_date").filter(is_published=True)[:3]
+
+    context = {"listings": listings}
+    return render(request, "pages/home.html", context)
+
 
 def page_about(request):
-    return render(request, 'pages/about.html')
+    realtors = Realtor.objects.all()
+    realtors_mvp = Realtor.objects.filter(is_mvp=True)
+    context = {
+        "realtors": realtors,
+        "realtors_mvp": realtors_mvp
+    }
+    return render(request, "pages/about.html", context)

32e3103cc99f5e8f8400894fb2e34019359958dc
Akshay Jain <jainakshay.aj98@gmail.com>
2021-06-12 11:11:23 +0530

    RE: partial work for search listings

--- pages/views.py
+++ pages/views.py
@@ -2,12 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from listings.models import Listing
 from realtors.models import Realtor
-
+from listings.choices import *
 
 def page_home(request):
     listings = Listing.objects.order_by("-list_date").filter(is_published=True)[:3]
 
-    context = {"listings": listings}
+
+    context = {
+        "listings": listings,
+        "state_choices": state_choices,
+        "bedroom_choices": bedroom_choices,
+        "price_choices": price_choices
+    }
     return render(request, "pages/home.html", context)
 
 
